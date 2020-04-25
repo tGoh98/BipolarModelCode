654b096560d279aab90eb2f90acfd1a669f5bf0b
tGoh98 <tmg5@rice.edu>
2020-04-25 01:09:46 -0500

    Added documentation

--- doFit.m
+++ doFit.m
@@ -1,5 +1,16 @@
 function desiredY = doFit(desiredX, currentY)
-    % ADD DOCUMENTATION
+    % doFit Interpolates currentY to fit desiredX.
+    %   Input:
+    %       currentY - Array of values.
+    %       desiredX - Array with number of desired elements.
+    %   Output:
+    %       desiredY - interpolated result of currentY
+    %           ode used.
+    %   Assumptions:
+    %       size(desiredX) >= currentY.
+    
+    % Calculate vector with same number of elements as currentY
     oldX = linspace(desiredX(1), desiredX(length(desiredX)), length(currentY));
-    desiredY = interp1(oldX, currentY, desiredX);    
+    % Interpolate
+    desiredY = interp1(oldX, currentY, desiredX);
 end
